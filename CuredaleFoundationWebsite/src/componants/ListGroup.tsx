f9cbb62b2335842a8a7bdff9781f9a6687069f69
Charlie <ccuredalerayner@gmail.com>
2024-10-01 11:51:54 +0100

    not sure i like this

--- CuredaleFoundationWebsite/src/componants/ListGroup.tsx
+++ CuredaleFoundationWebsite/src/componants/ListGroup.tsx
@@ -1,15 +1,10 @@
 function ListGroup() {
     let items = ["Cambridge", "Brigton", "Gwimpyvil", "Shropshire", "New York"];
-    items = [];
-
-    const getMessage = () => {
-        return items.length == 0 ? <p>No item found</p> : null;
-    };
 
     return (
         <>
             <h1>RSVP</h1>
-            {getMessage()}
+            {items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
                 {items.map((item) => (
                     <li key={item}>{item}</li>

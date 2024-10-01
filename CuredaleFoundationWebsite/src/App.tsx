7c59cbdece21e42e9bdfba6ffb07ebf8e6a847bd
Charlie <ccuredalerayner@gmail.com>
2024-10-01 12:33:57 +0100

    pass a func into a component

--- CuredaleFoundationWebsite/src/App.tsx
+++ CuredaleFoundationWebsite/src/App.tsx
@@ -3,9 +3,17 @@ import ListGroup from "./componants/ListGroup";
 function App() {
     let items = ["Cambridge", "Brigton", "Gwimpyvil", "Shropshire", "New York"];
 
+    const headleSelectItem = (item: string) => {
+        console.log(item);
+    };
+
     return (
         <div>
-            <ListGroup items={items} heading="RSVP" />
+            <ListGroup
+                items={items}
+                heading="RSVP"
+                onSelectItem={headleSelectItem}
+            />
         </div>
     );
 }

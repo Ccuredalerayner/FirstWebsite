68f5b081bd958ff2829cbdb0a28353b9219011c4
Charlie <ccuredalerayner@gmail.com>
2024-10-02 13:19:17 +0100

    hamburger

--- CuredaleFoundationWebsite/src/App.tsx
+++ CuredaleFoundationWebsite/src/App.tsx
@@ -1,16 +1,12 @@
 import { useState, useEffect } from "react";
 import MenuButton from "./componants/MenuButton";
 import ListGroup from "./componants/ListGroup";
+import Hamburger from "./componants/Hamburger";
 
 function App() {
     return (
-        <div className="col-4">
-            <MenuButton
-                btnColour={"primary"}
-                onClick={() => console.log("pressed")}
-            >
-                {"O"}
-            </MenuButton>
+        <div>
+            <Hamburger />
         </div>
     );
 }

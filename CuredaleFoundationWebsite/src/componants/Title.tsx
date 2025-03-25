8b76ebf88db68b279469857af22580bd94f969d0
Charlie <ccuredalerayner@gmail.com>
2025-03-25 12:36:21 +0000

    correction

--- CuredaleFoundationWebsite/src/componants/Title.tsx
+++ CuredaleFoundationWebsite/src/componants/Title.tsx
@@ -2,7 +2,7 @@ const Title = () => {
     return (
         <div className="title">
             <div className="title">
-                <h1>Stationary By</h1>
+                <h1>Stationery By</h1>
                 <h1>The Guest List</h1>
             </div>
             <style jsx="true">{`

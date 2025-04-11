3f67481b7b69faf10974c8f9dcaf62c54f307260
Charlie <ccuredalerayner@gmail.com>
2025-04-11 11:19:22 +0100

    menu in front of title page

--- src/componants/TitlePage.tsx
+++ src/componants/TitlePage.tsx
@@ -4,7 +4,7 @@ const TitlePage = () => {
     return (
         <>
             <div className="row position-relative">
-                <img src=".\images\shells_front_cover.jpg" className="img-fluid backup-image" alt="Responsive image" />
+                <img src=".\images\shells_front_cover.jpg" className="img-fluid back-image" alt="Responsive image" />
                 <div className="centered position-absolute">
                     <Title />
                 </div>
@@ -16,6 +16,9 @@ const TitlePage = () => {
                         transform: translate(-50%, -50%);
                         text-align: center
                         }
+                    .back-image {
+                        z-index: -1;
+                    }
                 `}
                 </style>
             </div>
